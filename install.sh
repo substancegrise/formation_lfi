5bf4e56c63a6a8a8a819f63a417beab9ff9048cb
Christian <substancegrise@gmail.com>
2016-08-21 17:05:53 +0200

    initialisation

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/env bash
+
+GULP='yes'
+USERNAME='root'
+PASSWORD='root'
+DBNAME='db_apero'
+HOST='localhost'
+
+USER_USERNAME='christian'
+USER_PASSWORD='collignon'
+
+MySQL=$(cat <<EOF
+DROP DATABASE IF EXISTS $DBNAME;
+CREATE DATABASE $DBNAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+DELETE FROM mysql.user WHERE user='$USER_USERNAME' and host='$USER_PASSWORD';
+GRANT ALL PRIVILEGES ON $DBNAME.* to '$USER_USERNAME'@'$HOST' IDENTIFIED BY '$USER_PASSWORD' WITH GRANT OPTION;
+EOF
+)
+
+echo $MySQL | mysql --user=$USERNAME --password=$PASSWORD
+
+php artisan migrate:refresh --seed
+
